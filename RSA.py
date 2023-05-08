771655b3052ff08591aa837eb61fd5108152b23b
saoebaba <117111950+saoebaba@users.noreply.github.com>
2023-05-09 01:04:23 +0530

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -1,38 +1,37 @@
 import math
 from cryptography.fernet import Fernet
 
-def RSA(a,h):
-    temp = 0
-    while(1):
-        temp=a%h
-        if(temp==0):
-            return h
-        a=h
-        h=temp
+def GCD(a,b):
+    if b==0:
+        return a
+    else:
+        return GCD(b,a%b)
+
 
 p=int(input("Enter your first prime number p: "))
 q=int(input("Enter your second prime number q: "))
 
 n=p*q
+
 e=2
+k=2
+
 phi=(p-1)*(q-1)
 
 while(e<phi):
-    if(RSA(e,phi)==1):
+    if(GCD(e,phi)==1):
         break
     else:
         e+=1
 
-k=2
+
 d=(1+(k*phi))/e
 
-print(d)
-
-msg=12.0
-
-print("Message data = ",msg)
+print(f"Public Key (e,n) : ({e},{n})")
+print(f"Private Key (d,n) : ({int(d)},{n})")
 
 a=input("Enter you msg : ")
+
 
 key=Fernet.generate_key()
 
