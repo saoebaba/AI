06e2671f66d13a93bd4da7f0bc611579cf6da450
saoebaba <117111950+saoebaba@users.noreply.github.com>
2023-05-28 23:41:58 +0530

    Update AES.py

--- AES.py
+++ AES.py
@@ -1,40 +1,20 @@
-import base64
+# Note : pip install pycryptodome
 from Crypto.Cipher import AES
-from Crypto import Random
-from Crypto.Protocol.KDF import PBKDF2
- 
-BLOCK_SIZE = 16
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
- 
-password = input("Enter encryption password: ")
- 
- 
-def get_private_key(password):
-    salt = b"this is a salt"
-    kdf = PBKDF2(password, salt, 64, 1000)
-    key = kdf[:32]
-    return key
- 
- 
-def encrypt(raw, password):
-    private_key = get_private_key(password)
-    raw = pad(raw)
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return base64.b64encode(iv + cipher.encrypt(raw))
- 
- 
-def decrypt(enc, password):
-    private_key = get_private_key(password)
-    enc = base64.b64decode(enc)
-    iv = enc[:16]
-    cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return unpad(cipher.decrypt(enc[16:]))
- 
+from Crypto.Util.Padding import pad, unpad
+key = b'1234567887654321'
+data = b'sinhgad academy'
+padded_data = pad(data, AES.block_size)
 
-encrypted = encrypt("This is a secret message", password)
-print(encrypted)
+cipher = AES.new(key, AES.MODE_ECB)
 
-decrypted = decrypt(encrypted, password)
-print(bytes.decode(decrypted))
+encrypted_data = cipher.encrypt(padded_data)
+
+decrypted_data = cipher.decrypt(encrypted_data)
+
+unpadded_data = unpad(decrypted_data, AES.block_size)
+
+print("Original Data:", data)
+print("Padded Data:", padded_data)
+print("Encrypted Data:", encrypted_data)
+print("Decrypted Data:", decrypted_data)
+print("Unpadded Data:",unpadded_data)
