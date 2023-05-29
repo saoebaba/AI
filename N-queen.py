2c1e9b5e098e8a354573a6e277bb143a09a625ec
saoebaba <117111950+saoebaba@users.noreply.github.com>
2023-05-29 21:44:46 +0530

    Update N-queen.py

--- N-queen.py
+++ N-queen.py
@@ -1,63 +1,32 @@
-N = 4
+print("Enter the number of queens: ")
+N=int(input())
 
-def printSolution(board):
-	for i in range(N):
-		for j in range(N):
-			print(board[i][j], end = " ")
-		print()
-
-def isSafe(row, col, slashCode, backslashCode,rowLookup, slashCodeLookup,backslashCodeLookup):
-	if (slashCodeLookup[slashCode[row][col]] or backslashCodeLookup[backslashCode[row][col]] or rowLookup[row]):
-		return False
-	return True
-
-def solveNQueensUtil(board, col, slashCode, backslashCode,rowLookup, slashCodeLookup,backslashCodeLookup):
-						
-	if(col >= N):
-		return True
-	for i in range(N):
-		if(isSafe(i, col, slashCode, backslashCode,rowLookup, slashCodeLookup,backslashCodeLookup)):
-					
-			board[i][col] = 1
-			rowLookup[i] = True
-			slashCodeLookup[slashCode[i][col]] = True
-			backslashCodeLookup[backslashCode[i][col]] = True
-			
-			if(solveNQueensUtil(board, col + 1,slashCode, backslashCode,rowLookup, slashCodeLookup,backslashCodeLookup)):
-				return True
-			
-			board[i][col] = 0
-			rowLookup[i] = False
-			slashCodeLookup[slashCode[i][col]] = False
-			backslashCodeLookup[backslashCode[i][col]] = False
-			
-	return False
-
-def solveNQueens():
-	board = [[0 for i in range(N)]
-	  			for j in range(N)]
-	
-	slashCode = [[0 for i in range(N)]
-					for j in range(N)]
-	backslashCode = [[0 for i in range(N)]
-						for j in range(N)]
-	
-	rowLookup = [False] * N
-	
-	x = 2 * N - 1
-	slashCodeLookup = [False] * x
-	backslashCodeLookup = [False] * x
-	
-	for rr in range(N):
-		for cc in range(N):
-			slashCode[rr][cc] = rr + cc
-			backslashCode[rr][cc] = rr - cc + (N-1)
-	
-	if(solveNQueensUtil(board, 0, slashCode, backslashCode,rowLookup, slashCodeLookup,backslashCodeLookup) == False):
-		print("Solution does not exist")
-		return False
-		
-	printSolution(board)
-	return True
-
-solveNQueens()
+#Chessboard:
+board=[[0]*N for _ in range(N)]
+def is_attack(i,j):
+    for k in range(0,N):
+        if board [i][k]==1 or board[k][j]==1:
+            return True
+    #Checking Diagonals:
+    for k in range(0,N):
+        for l in range(0,N):
+            if(k+l==i+j) or (k-l==i-j):
+                if board[k][l]==1:
+                    return True
+    return False
+def N_queen(n):
+    if n==0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
+            '''checking if we can place a queen here or no queen wi'''
+            if (not(is_attack(i,j))) and (board[i][j]!=1):
+                board[i][j]=1
+                #Recursion
+                if N_queen(n-1)==True:
+                    return True
+                board[i][j]=0
+    return False
+N_queen(N)
+for i in board:
+    print(i)
